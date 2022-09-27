53ec87c7279010f7246f88b1ca94f84e75f23693
ReYe0 <1299461580@qq.com>
2022-09-27 10:25:37 +0800

    修改了 .gitignore

--- _03.spring-boot-mybatis/src/main/java/com/study/TestController/TestController.java
+++ _03.spring-boot-mybatis/src/main/java/com/study/TestController/TestController.java
@@ -3,6 +3,7 @@ package com.study.TestController;
 import com.study.entity.Student;
 import com.study.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,8 @@ public class TestController {
 	@Autowired
 	private StudentService studentService;
 	
-	@RequestMapping( value = "/querystudent", method = RequestMethod.GET)
-	public Student queryStudentBySno(String sno) {
+	@RequestMapping( value = "/querystudent/{sno}", method = RequestMethod.GET)
+	public Student queryStudentBySno(@PathVariable("sno") String sno) {
 		return this.studentService.queryStudentBySno(sno);
 	}
 }
