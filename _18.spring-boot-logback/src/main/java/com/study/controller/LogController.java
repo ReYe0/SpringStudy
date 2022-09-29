c762d56ee37decca52e141c288c1d55a3d61e92e
ReYe0 <1299461580@qq.com>
2022-09-29 15:03:23 +0800

    logback 完成

--- _18.spring-boot-logback/src/main/java/com/study/controller/LogController.java
+++ _18.spring-boot-logback/src/main/java/com/study/controller/LogController.java
@@ -1,25 +1,27 @@
 package com.study.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j(topic = "LogController")
 public class LogController {
     // 重点
     Logger logger = LoggerFactory.getLogger("LogController");
     @GetMapping("/recommend")
     public String index() {
 
-//        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000; i++) {
             logger.trace("A TRACE Message");
             logger.debug("A DEBUG Message");
             logger.info("An INFO Message");
             logger.warn("A WARN Message");
             logger.error("An ERROR Message");
-//        }
+        }
 
         return "Greetings from Spring Boot!";
 
